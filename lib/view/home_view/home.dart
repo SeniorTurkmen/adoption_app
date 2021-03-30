13affe7a8e884651eb280be4a493a1ec49ba205c
Mustafa <mustafa.trkmn.ceng@gmail.com>
2021-03-30 12:41:16 +0300

    Nearby Pet Section complete

--- lib/view/home_view/home.dart
+++ lib/view/home_view/home.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:adoption_app/core/constants/color_constants.dart';
 import 'package:adoption_app/core/util/extension/context_extensions.dart';
 import 'package:adoption_app/view/home_view/view_model/functions/button_functions.dart';
 import 'package:adoption_app/view/home_view/view_model/home_view_model.dart';
@@ -12,32 +15,133 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).canvasColor,
-        shadowColor: Colors.transparent,
-        leading: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: SvgPicture.asset('svg/paw.svg')),
-      ),
-      body: Consumer<HomeViewModel>(
-        builder: (ctx, home, child) => Container(
-          padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * .02),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              HeaderWidget(),
-              SearchBar(),
-              PetTypeButtons(
-                home: home,
-              )
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).canvasColor,
+          shadowColor: Colors.transparent,
+          leading: SvgPicture.asset('svg/paw.svg'),
+        ),
+        body: Consumer<HomeViewModel>(
+          builder: (ctx, home, child) => Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HeaderWidget(),
+                SearchBar(),
+                PetTypeButtons(
+                  home: home,
+                ),
+                Expanded(
+                    child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Nearby',
+                          style: context.textTheme.headline5!
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Text(
+                            'SEE ALL',
+                            style: context.textTheme.bodyText1!.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: ColorPalette.instance.electricViolet),
+                          ),
+                        )
+                      ],
+                    ),
+                    Expanded(
+                      child:
+                          ListView(scrollDirection: Axis.horizontal, children: [
+                        PetCard(),
+                        PetCard(),
+                        PetCard(),
+                      ]),
+                    )
+                  ],
+                ))
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+}
+
+class PetCard extends StatelessWidget {
+  const PetCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: context.width * 1,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [ImageCard(), nameOfPet(context), awayFrom()],
+      ),
+    );
+  }
+
+  Text nameOfPet(BuildContext context) {
+    return Text(
+      'Akira',
+      style: context.textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold),
+    );
+  }
+
+  Row awayFrom() {
+    return Row(
+      children: [SvgPicture.asset('svg/navigation.svg'), Text(' 0.7km away')],
+    );
+  }
+}
+
+class ImageCard extends StatelessWidget {
+  const ImageCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: _decoration(),
+      child: _clipRectangle(),
+    );
+  }
+
+  ClipRRect _clipRectangle() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: _image(),
+    );
+  }
+
+  Image _image() {
+    return Image.asset(
+      'image/cat1.png',
+      fit: BoxFit.fill,
+    );
+  }
+
+  BoxDecoration _decoration() {
+    return BoxDecoration(boxShadow: <BoxShadow>[
+      BoxShadow(
+          color: ColorPalette.instance.blackcurrant.withOpacity(.2),
+          blurRadius: 15,
+          spreadRadius: 1)
+    ]);
   }
 }
 
